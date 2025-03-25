e57256447430bafe4922482f4043623ddd6356fa
Andreas Goss <andreas.goss1994@gmail.com>
2025-03-25 13:52:58 +0100

    Adjust Gitsign with traffic light color scheme

--- ftplugin/json.lua
+++ ftplugin/json.lua
@@ -1,3 +1,2 @@
 vim.bo.formatexpr = ''
 vim.bo.formatprg = 'jq'
-print 'Json opened'
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,20 +1,29 @@
 -- Adds git related signs to the gutter, as well as utilities for managing changes
 return {
   'lewis6991/gitsigns.nvim',
-  opts = {
-    signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
-    signs_staged = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
-  },
+  config = function()
+    local gitsigns = require 'gitsigns'
+    gitsigns.setup {
+      signs = {
+        add = { text = '┃' },
+        change = { text = '┃' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked = { text = '┆' },
+      },
+      signs_staged = {
+        add = { text = '┃' },
+        change = { text = '┃' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked = { text = '┆' },
+      },
+    }
+
+    vim.api.nvim_set_hl(0, 'GitSignsAdd', { fg = '#2dc937', bold = true })
+    vim.api.nvim_set_hl(0, 'GitSignsChange', { fg = '#db7b2b', bold = true })
+    vim.api.nvim_set_hl(0, 'GitSignsDelete', { fg = '#cc3232', bold = true })
+  end,
 }
