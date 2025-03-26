c7c03f7abfbf6a366195fd65e68673aefc725cbc
Andreas Goss <andreas.goss1994@gmail.com>
2025-03-26 21:50:17 +0100

    Adjust ToggleTerm config

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,7 +1,46 @@
 return {
   'akinsho/toggleterm.nvim',
   version = '*',
-  opts = {
-    open_mapping = [[<c-\>]],
-  },
+  config = function()
+    local toggleterm = require 'toggleterm'
+    toggleterm.setup {
+      size = function(term)
+        if term.direction == 'horizontal' then
+          return 15
+        elseif term.direction == 'vertical' then
+          return vim.o.columns * 0.4
+        end
+      end,
+      open_mapping = [[<c-\>]], -- or { [[<c-\>]], [[<c-¥>]] } if you also use a Japanese keyboard.
+      hide_numbers = true, -- hide the number column in toggleterm buffers
+      shade_filetypes = {},
+      autochdir = false, -- when neovim changes it current directory the terminal will change it's own when next it's opened
+      shade_terminals = true, -- this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
+      shading_factor = -30, -- the percentage by which to lighten dark terminal background, default: -30
+      shading_ratio = -3, -- the ratio of shading factor for light/dark terminal background, default: -3
+      start_in_insert = true,
+      insert_mappings = true, -- whether or not the open mapping applies in insert mode
+      terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
+      persist_size = true,
+      persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
+      direction = 'horizontal',
+      close_on_exit = true, -- close the terminal window when the process exits
+      clear_env = false, -- use only environmental variables from `env`, passed to jobstart()
+      -- Change the default shell. Can be a string or a function returning a string
+      shell = vim.o.shell,
+      auto_scroll = true, -- automatically scroll to the bottom on terminal output
+    }
+    function _G.set_terminal_keymaps()
+      local opts = { buffer = 0 }
+      vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+      vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+      vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+      vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+      vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+      vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+      vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+    end
+    -- if you only want these mappings for toggle term use term://*toggleterm#* instead
+    vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()'
+  end,
 }
