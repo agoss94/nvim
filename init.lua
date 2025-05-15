7fc4bcb0043b60bccd6cd194a204964b55f09a3e
Andreas Goss <agoss@itemis.com>
2025-05-15 08:26:46 +0200

    Add lilypond suite as plugin

--- init.lua
+++ init.lua
@@ -39,5 +39,5 @@ require('lazy').setup {
   require 'plugins.indent-blankline',
   require 'plugins.toggleterm',
   require 'plugins.misc',
-  require 'plugins.rest',
+  require 'plugins.lilypond-suite',
 }
